136cf97b58eff9735c5f87f5b2a685412d0a9a6f
emmeiwhite <emmeiwhite@gmail.com>
2025-02-02 14:51:58 +0530

    react-1.0 tailwind added to vite project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  // UI is dependant on data of the application called the state
+  return (
+    <section className="container px-5">
+      <button className="bg-blue-500">I am a button</button>
+    </section>
+  )
+}
+
+export default App
