9654a91e5488325cc1c23a7e1f8ec7f0f326dedd
emmeiwhite <emmeiwhite@gmail.com>
2025-02-02 16:34:06 +0530

    react-14 step functionality worked fine!

--- src/components/DateCounter.jsx
+++ src/components/DateCounter.jsx
@@ -1,4 +1,16 @@
+import { useState } from 'react'
+
 const DateCounter = () => {
+  const [step, setStep] = useState(0)
+  const [count, setCount] = useState(0)
+
+  function handleCountIncrease() {
+    setCount(c => (c + !step ? c + 1 : step))
+  }
+
+  function handleCountDecrease() {
+    setCount(c => (c - !step ? c - 1 : step))
+  }
   return (
     <div>
       <h2 className="text-center my-3 text-2xl font-semibold">
@@ -6,26 +18,35 @@ const DateCounter = () => {
       </h2>
 
       <div className="flex gap-3 justify-center items-center">
-        <button className="px-3 py-1 bg-orange-400 text-white cursor-pointer hover:bg-orange-600 transition text-white">
+        <button
+          className="px-3 py-1 bg-orange-400 text-white cursor-pointer hover:bg-orange-600 transition text-white"
+          onClick={() => setStep(c => c - 1)}>
           -
         </button>
-        <p>Step: 5</p>
-        <button className="px-3 py-1 bg-orange-400 text-white cursor-pointer hover:bg-orange-600 transition text-white">
+        <p>Step: {step}</p>
+        <button
+          className="px-3 py-1 bg-orange-400 text-white cursor-pointer hover:bg-orange-600 transition text-white"
+          onClick={() => setStep(c => c + 1)}>
           +
         </button>
       </div>
 
       <div className="flex gap-3 justify-center items-center mt-3">
-        <button className="px-3 py-1 bg-teal-400 hover:bg-teal-600 transition text-white cursor-pointer">
+        <button
+          className="px-3 py-1 bg-teal-400 hover:bg-teal-600 transition text-white cursor-pointer"
+          onClick={handleCountDecrease}>
           -
         </button>
-        <p>Count: 0</p>
-        <button className="px-3 py-1 bg-teal-400 hover:bg-teal-600 transition text-white cursor-pointer">
+        <p>Count: {count}</p>
+        <button
+          className="px-3 py-1 bg-teal-400 hover:bg-teal-600 transition text-white cursor-pointer"
+          onClick={handleCountIncrease}>
           +
         </button>
       </div>
 
       <p className="mt-4">Today is Mon Jun 21, 2024</p>
+      <h3>Current Count Value: {count}</h3>
     </div>
   )
 }
