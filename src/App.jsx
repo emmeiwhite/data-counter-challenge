bac8c616f05ffb171b1fc419813cac3df26f5331
emmeiwhite <emmeiwhite@gmail.com>
2025-02-02 15:17:18 +0530

    react-1.1 tailwind testing --- working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
   // UI is dependant on data of the application called the state
   return (
-    <section className="container px-5">
-      <button className="bg-blue-500">I am a button</button>
+    <section className="container px-5 pt-5 text-center">
+      <h1 className="text-3xl my-6">Setting up Tailwind-4 with React-Vite Project</h1>
+      <button className="bg-blue-500 p-2 text-white">I am a button</button>
     </section>
   )
 }
