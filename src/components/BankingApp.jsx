659e5e927413d2a8b39a471fb13cbf55eac928e5
emmeiwhite <emmeiwhite@gmail.com>
2025-02-09 00:41:25 +0530

    useReducer-2.9 banking assignment complete!

--- src/components/BankingApp.jsx
+++ src/components/BankingApp.jsx
@@ -18,18 +18,36 @@ const reducer = (state, action) => {
   if (action.type === 'deposit') {
     return {
       ...state,
-      isDisabled: false,
       balance: state.balance + 150
     }
   }
 
   if (action.type === 'withdraw') {
-    if (state.balance <= 50) return { ...state }
+    if (state.balance <= action.payload) return { ...state }
 
     return {
       ...state,
-      isDisabled: false,
-      balance: state.balance - 50
+      balance: state.balance - action.payload
+    }
+  }
+
+  if (action.type === 'request_loan') {
+    return {
+      ...state,
+      loan: state.loan + action.payload
+    }
+  }
+
+  if (action.type === 'pay_loan') {
+    return {
+      ...state,
+      loan: 0
+    }
+  }
+
+  if (action.type === 'close_account') {
+    return {
+      ...initialState
     }
   }
 }
@@ -47,8 +65,11 @@ const BankingApp = () => {
 
       <div className="my-4">
         <button
-          className="px-4 py-2 bg-blue-400 text-white cursor-pointer"
-          onClick={handleOpen}>
+          className={`px-4 py-2 bg-blue-400 text-white cursor-pointer ${
+            state.isDisabled ? 'border-2 border-amber-700 cursor-pointer' : ' cursor-not-allowed'
+          }`}
+          onClick={handleOpen}
+          disabled={!state.isDisabled}>
           Open Account
         </button>
       </div>
@@ -70,7 +91,7 @@ const BankingApp = () => {
             state.isDisabled ? 'cursor-not-allowed' : 'border-2 border-red-700 cursor-pointer'
           }`}
           disabled={state.isDisabled}
-          onClick={() => dispatch({ type: 'withdraw' })}>
+          onClick={() => dispatch({ type: 'withdraw', payload: 50 })}>
           Withdraw 50
         </button>
       </div>
@@ -80,7 +101,8 @@ const BankingApp = () => {
           className={`px-4 py-2 bg-purple-400 text-white ${
             state.isDisabled ? 'cursor-not-allowed' : 'border-2 border-green-700 cursor-pointer'
           }`}
-          disabled={state.isDisabled}>
+          disabled={state.isDisabled}
+          onClick={() => dispatch({ type: 'request_loan', payload: 5000 })}>
           Request a loan of 5000
         </button>
       </div>
@@ -90,7 +112,8 @@ const BankingApp = () => {
           className={`px-4 py-2 bg-purple-400 text-white ${
             state.isDisabled ? 'cursor-not-allowed' : 'border-2 border-red-700 cursor-pointer'
           }`}
-          disabled={state.isDisabled}>
+          disabled={state.isDisabled}
+          onClick={() => dispatch({ type: 'pay_loan' })}>
           Pay back full loan
         </button>
       </div>
@@ -100,7 +123,8 @@ const BankingApp = () => {
           className={`px-4 py-2 bg-black text-white ${
             state.isDisabled ? 'cursor-not-allowed' : 'border-2 border-green-700 cursor-pointer'
           }`}
-          disabled={state.isDisabled}>
+          disabled={state.isDisabled}
+          onClick={() => dispatch({ type: 'close_account' })}>
           Close the account
         </button>
       </div>
